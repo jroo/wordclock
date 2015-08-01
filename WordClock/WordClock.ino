151c36b142f143112cb9d746cfc2b5ec1e0675dc
jroo <ruihley@gmail.com>
2015-07-31 23:03:43 -0400

    - updated license and revision history - moved sketch into WordClock folder - added .gitignore

--- WordClock/WordClock.ino
+++ WordClock/WordClock.ino
@@ -1,6 +1,6 @@
 
 
-/**************************************************************************
+/***************************************************************************
  *                                                                         *
  *  W O R D C L O C K   - A clock that tells the time using words.         *
  *                                                                         *
@@ -11,31 +11,30 @@
  *   Modifications Copyright (C) 2010 Scott Bezek (scott@bezekhome.com)    *
  *                                                                         *
  ***************************************************************************
- *                                                                         * 
- * This program is free software; you can redistribute it and/or modify    *
- * it under the terms of the GNU General Public License as published by    *
- * the Free Software Foundation; either version 2 of the License, or       *
- * (at your option) any later version.                                     *
  *                                                                         *
- * This program is distributed in the hope that it will be useful,         *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
- * GNU General Public License for more details.                            *
+ *   This program is free software: you can redistribute it and/or modify  *
+ *    it under the terms of the GNU General Public License as published by *
+ *    the Free Software Foundation, either version 3 of the License, or    *
+ *    (at your option) any later version.                                  *
  *                                                                         *
- * You should have received a copy of the GNU General Public License       *
- * along with this program; if not, write to the Free Software             *
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston,                   *
- * MA  02111-1307  USA                                                     *
+ *    This program is distributed in the hope that it will be useful,      *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ *    GNU General Public License for more details.                         *
+ *                                                                         *
+ *    You should have received a copy of the GNU General Public License    *
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.*
  *                                                                         *
  ***************************************************************************
  * 
  * Revision History
  * 
- * Date  	By	What
- * 20001025	DRJ	Initial Creation of Arduino Version 
- *                      - based on Wordclock.c - from PIC version
- * 20100124  Scott Bezek Changed LED pinout, added brightness control,
- *                        changed buttons to hour/minute increment 
+ * Date         By          What
+ * 20001025     DRJ         Initial Creation of Arduino Version 
+ *                              - based on Wordclock.c - from PIC version
+ * 20100124     Scott Bezek Changed LED pinout, added brightness control,
+ *                              changed buttons to hour/minute increment 
+ * 20150731     jroo        added four LEDs for minute display
  */
 
 
